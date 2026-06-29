7fd7227f2d4631578870d59c7e2804fec65d5bea
xuezhaju <liaozecheng123@163.com>
2026-06-29 17:42:07 +0800

    chore: 停止跟踪 build 目录，并更新 .gitignore

--- build/CMakeFiles/TUI-MCLauncher.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for TUI-MCLauncher.
-# This may be replaced when dependencies are built.
--- build/CMakeFiles/TUI-MCLauncher.dir/src/ui/hub.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/TUI-MCLauncher.dir/src/ui/hub.cpp.o: \
- /home/l0rax/Projects/Program/CPP/TUI-MCLauncher/src/ui/hub.cpp \
- /usr/include/stdc-predef.h
